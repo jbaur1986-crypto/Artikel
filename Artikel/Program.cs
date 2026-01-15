8cff79faea4499e6d5d2ecea8e28c2444ab9d7d4
Johannes Baur <j.baur1986@gmail.com>
2026-01-15 20:41:19 +0100

    V01

---
+++ Artikel/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Artikel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+enum ArtTyp 
+{
+    Sonstiges,
+    Bekleidung,
+    Haushalt,
+    Einrichtung
+}
+
+class Artikel
+{
+    private int artNr;
+    private string artName;
+    private decimal artPreis;
+
+    public ArtTyp meinTyp = ArtTyp.Sonstiges;
+
+    public Artikel(int artNr, string artName, decimal artPreis, ArtTyp welcherTyp)
+    {
+        this.artNr = artNr;
+        this.artName = artName;
+        this.artPreis = artPreis;
+        meinTyp = welcherTyp;
+    }
+    
+    public void SetPrice()
+    {
+        //Setter muss noch geschrieben werden.
+    }
+    
+    // Getter für GetPreis, Getter für GetType; außerdem d, e
+}
